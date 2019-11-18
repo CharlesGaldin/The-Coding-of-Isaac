1752899e832d26c10bf959b19d0fc1c74790551d
Paul Guiton <guiton.paul@gmail.com>
2019-11-18 10:29:53 +0100

    Added stub for Game()

---
+++ game/__init__.py
@@ -0,0 +1,14 @@
+
+GRID_SIZE = 15
+
+class Game:
+	def __init__(self):
+		self.grid = [[None]*GRID_SIZE for i in range(GRID_SIZE)]
+	
+	def run():
+		print("Hello, world!")
+
+
+if __name__ == "__main__":
+	game = Game()
+	game.run()
