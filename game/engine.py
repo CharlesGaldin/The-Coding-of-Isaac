2ab87467eeb174f149a9b1b86f50ca9b66fa82ae
Charles Galdin <charles.galdin@student-cs.fr>
2019-11-18 10:38:42 +0100

    changed spaces inton tabs

--- game/engine.py
+++ game/engine.py
@@ -1,12 +1,12 @@
 def init_grid(size):
-    grid = []
-    for i in range(size):
-        grid += [[]]
-        for j in range(size):
-            grid[i] += [[None]]
-    return grid
+	grid = []
+	for i in range(size):
+		grid += [[]]
+		for j in range(size):
+			grid[i] += [[None]]
+	return grid
 
 def init_grid_level(level):
-    if level == 1:
-        return init_grid(15)
+	if level == 1:
+		return init_grid(15)
 
