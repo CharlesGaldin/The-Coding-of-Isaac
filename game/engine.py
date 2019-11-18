7fea19456863825742ba06cb082638377618bfdb
Girard-Carrabin Baptiste <baptiste.gc@student-cs.fr>
2019-11-18 16:20:30 +0100

    check move

--- game/engine.py
+++ game/engine.py
@@ -1,17 +1,17 @@
-def init_grid(size):
+"""def init_grid(size):
     	grid = [[None for i in range(size)] for j in range(size)]
 	return grid
 
 def init_grid_level(level):
 	if level == 1:
 		return init_grid(15)
-
-from game.entity import Player, Monster
+"""
+from entity import Player, Monster
 
 def player_placement(grid):
 	size=len(grid)
 	p=size//2
-	grid[p][p] = Player(10)
+	grid[p][p] = Player([p , p])
 	return grid
 
 def update(grid):
@@ -19,14 +19,14 @@ def update(grid):
 
 def move_entity(entity, direction):
 	pos = [entity.pos[0],entity.pos[1]]
-	if move == 'up':
+	if direction == 'up':
 		if pos[0]==0:
 			return 'move not possible'
 		else:
 			entity.pos[0] -= 1
-	elif move == 'down':
+	elif direction == 'down':
 		return move_entity(entity, up)
-	elif move == 'left':
+	elif direction == 'left':
 		if pos[1]==0:
 			return 'move not possible'
 		else:
