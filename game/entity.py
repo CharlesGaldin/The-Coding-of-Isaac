7cfbd97378176a8571458a622b73f4a23f253954
Girard-Carrabin Baptiste <baptiste.gc@student-cs.fr>
2019-11-18 11:39:24 +0100

    modify attributes

--- game/entity.py
+++ game/entity.py
@@ -1,12 +1,14 @@
 class Entity:
 
-	def __init__(self):
-		pass
+	def __init__(self , position , health):
+		self.position = position
+		self.health = health
 
 class Player(Entity):
 
 	def __init__(self):
 		pass
+
 class Monster(Entity):
 
 	def __init__(self):
