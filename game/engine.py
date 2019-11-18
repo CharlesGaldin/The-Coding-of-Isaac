add0a9dee2a849b581af5f110e3d9c2c13dac3af
Charles Galdin <charles.galdin@student-cs.fr>
2019-11-18 11:02:08 +0100

    modified grid generation

--- game/engine.py
+++ game/engine.py
@@ -2,11 +2,9 @@ def init_grid(size):
 	grid = []
 	for i in range(size):
 		grid += [[]]
-		for j in range(size):
-			grid[i] += [[None]]
+		grid[i] += [[None]]
 	return grid
 
 def init_grid_level(level):
 	if level == 1:
 		return init_grid(15)
-
