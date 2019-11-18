b5246c63c3641c99e3e721204f3507afb7c3db57
Charles Galdin <charles.galdin@student-cs.fr>
2019-11-18 16:19:24 +0100

    finished player movement

--- game/engine.py
+++ game/engine.py
@@ -1,16 +1,17 @@
-def init_grid(size):
-    	grid = [[None for i in range(size)] for j in range(size)]
+from game.entity import Player, Monster
+
+GRID_SIZE = 15
+
+def init_grid():
+    	grid = [[None for i in range(GRID_SIZE)] for j in range(GRID_SIZE)]
 	return grid
 
 def init_grid_level(level):
 	if level == 1:
 		return init_grid(15)
 
-from game.entity import Player, Monster
-
 def player_placement(grid):
-	size=len(grid)
-	p=size//2
+	p=GRID_SIZE//2
 	grid[p][p] = Player(10)
 	return grid
 
@@ -21,13 +22,23 @@ def move_entity(entity, direction):
 	pos = [entity.pos[0],entity.pos[1]]
 	if move == 'up':
 		if pos[0]==0:
-			return 'move not possible'
+			raise NameError('Move not possible')
 		else:
 			entity.pos[0] -= 1
 	elif move == 'down':
-		return move_entity(entity, up)
+		if pos[0] == GRID_SIZE-1:
+			raise NameError('Move not possible')
+		else:
+    		entity.pos[0] += 1
 	elif move == 'left':
 		if pos[1]==0:
-			return 'move not possible'
+			raise NameError('Move not possible')
 		else:
 			entity.pos[1] -= 1
+	elif move == 'right':
+		if pos[1]==GRID_SIZE-1:
+    		raise NameError('Move not possible')
+		else:
+			entity.pos[1] += 1
+	else:
+		raise NameError('Attribute not recogized, please choose between "right", "left", "up" and "down"')
