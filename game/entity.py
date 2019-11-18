09d75e9e599fbb1fbd36cd49fe73673f9a0b1d86
Girard-Carrabin Baptiste <baptiste.gc@student-cs.fr>
2019-11-18 14:52:16 +0100

    add link Trello and change_helth

--- game/entity.py
+++ game/entity.py
@@ -1,14 +1,30 @@
-class Entity:
+class Moving_Entity:
+    
+	def __init__(self , health ):
+		self.health = health
+		#self.artwork = artwork
+	
+	def health_change(self , n):
+    	# n is an integer
+		# add n to the health of an entity
+		self.health += n
+
+class Player(Moving_Entity):
 
 	def __init__(self , health):
-		self.health = health
+		Moving_Entity.__init__(self , health )
 
-class Player(Entity):
+class Monster(Moving_Entity):
+
+	def __init__(self):
+		pass
+
+class Unmoving_entity:
 
 	def __init__(self):
 		pass
 
-class Monster(Entity):
 
-	def __init__(self):
-		pass
+unit = Player(10)
+unit.health_change(2)
+print(unit.health)
