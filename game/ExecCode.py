ab593633c9406c652d04c76dc4b078ed6d1e0652
unknown <Odysseas@I3579A.acial.loc>
2019-11-20 09:57:48 +0100

    import de la fonction move dans ExecCode rajout sur gitignore

--- game/ExecCode.py
+++ game/ExecCode.py
@@ -1,6 +1,8 @@
-def codeJoueur(player, userCode):
+from game.engine import move_entity
+
+def codeJoueur(player, userCode, grille):
     def pos() :
         p = player.pos()
         return tuple(p)
-    correspondances = {"move" : lambda direction : move_entity(player, grille, direction), "getPos" : lambda : pos}
+    correspondances = {"move" : lambda direction : move_entity(player, direction), "getPos" : lambda : pos}
     exec(userCode, correspondances)
