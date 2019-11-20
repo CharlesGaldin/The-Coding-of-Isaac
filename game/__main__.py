a1926f10f13baa502fe7d97a41493265ff1dfaee
Paul Guiton <guiton.paul@gmail.com>
2019-11-20 10:22:44 +0100

    Integrated pygame into Tk frame

--- game/__main__.py
+++ game/__main__.py
@@ -1,7 +1,10 @@
 from game.engine import init_grid, player_placement
-from game.graphics import create_window, load_images, display_map
+from game.graphics import get_window_size, create_window, load_images, display_map
 import pygame
 import pygame.locals
+import tkinter as tk
+import os
+import platform
 
 import threading
 from game.editor import EditorSetUp
@@ -10,15 +13,19 @@ class Game:
 	def __init__(self):
 		self.grid = init_grid()
 		self.player = player_placement(self.grid)
-
+		
+		self.editor = EditorSetUp()
+		
+		win_w, win_h = get_window_size(self.grid, 30)
+		self.pygame_frame = tk.Frame(self.editor.master, width = win_w, height = win_h)
+		self.pygame_frame.pack(side = tk.LEFT)
+		
+		os.environ['SDL_WINDOWID'] = str(self.pygame_frame.winfo_id())
+		if platform.system == "Windows":
+			os.environ['SDL_VIDEODRIVER'] = 'windib'
 		self.window = create_window(self.grid, 30)
+		
 		self.images = load_images()
-		
-		def tkthread():
-			self.editor = EditorSetUp()
-			self.editor.run()
-		
-		threading.Thread(target=tkthread).start()
 	
 	def run(self):
 		running = True
@@ -26,11 +33,16 @@ class Game:
 		while running:
 			pygame.time.Clock().tick(60)
 			
+			self.editor.update()
+			if self.editor.exit_requested:
+				running = False
+			
 			for event in pygame.event.get():
 				if event.type == pygame.locals.QUIT:
 					running = False
 			pygame.display.flip()
 		pygame.quit()
+		self.editor.close()
 
 
 if __name__ == "__main__":
