0493f4434d6c5ebf7f255224e4409db179bf1eaf
Girard-Carrabin Baptiste <baptiste.gc@student-cs.fr>
2019-11-20 09:34:22 +0100

    add attack feature

--- game/entity.py
+++ game/entity.py
@@ -1,10 +1,12 @@
 class Moving_Entity:
 	
-	def __init__(self , health , position , artwork):
+	def __init__(self , position , health , attack , artwork):
 
 		self.health = health
 		self.pos = position
 		self.artwork = artwork
+		self.moved = True
+		self.attack = attack
 	
 	def health_change(self , n):
 		"""
@@ -12,28 +14,34 @@ class Moving_Entity:
 		add n to the health of an entity
 		"""
 		self.health += n
-			
+
+	def attack_enemy(self , target):
+    		health_change(target , -self.attack)		
 
 class Player(Moving_Entity):
 
-	def __init__(self , position , health = 10 ):
+	def __init__(self , position , health = 10 , attack = 2 ):
 		"""
 		position de la forme : [pos_y , pos_x]
 		"""
-		Moving_Entity.__init__(self , position , health, "robot")
+		Moving_Entity.__init__(self , position , health , attack , "robot")
 
 class Monster(Moving_Entity):
 	
-	def __init__(self , health , artwork):
+	def __init__(self , position , health , attack , artwork):
 		"""
 		position de la forme : [pos_y , pos_x]
 		"""
-		Moving_Entity.__init__(self , health , artwork)
+		Moving_Entity.__init__(self , position ,  health , attack , artwork)
 
 class Unmoving_entity:
 
-	def __init__(self):
-		pass
+	def __init__(self , position):
+		self.position = position
+
+class Objective:
+    	
+	def __init__()
 
 class Obstacle:
 	def __init__(self):
